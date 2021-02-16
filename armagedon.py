54098f880a7b3f24dbc436a0707a782fe2ef791c
romanmajorov <57148482+romanmajorov@users.noreply.github.com>
2021-02-16 20:36:42 +0300

    Add files via upload

---
+++ armagedon.py
@@ -0,0 +1,37 @@
+import requests, random, datetime, sys, time, os
+
+from termcolor import colored
+from modules import logo
+
+def russia():
+    os.system("cd bombers && python3 bomber.py")
+
+def batya():
+    print(logo.batya)
+    input()
+    main()
+
+def update():
+    os.system("cd modules && python3 update.py")
+
+def exit():
+    os.system("clear")
+    print('Goodby :(')
+    sys.exit()
+
+def main():
+    os.system('clear')
+    print(logo.banner)
+    print(logo.menu)
+    num_menu = input("iisus> ")
+    if num_menu == "":
+       main()
+    if num_menu == "1":
+        russia()
+    if num_menu == "2":
+        batya()
+    if num_menu == "3":
+        update()
+    if num_menu == "4":
+        exit()
+main()
